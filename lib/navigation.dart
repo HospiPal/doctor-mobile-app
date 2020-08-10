07d8e55883be2c9f03c73e52eee6d7cba90825c6
Kenny Newman <knewman55@gmail.com>
2020-08-10 01:06:26 -0400

    Added care team feature, as well as ability to filter journal logs by year
    Signed-off-by: Kenny <knewman55@gmail.com>

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -1,9 +1,21 @@
+import 'package:doctorapp/central_screen/care_team/profile.dart';
+import 'package:doctorapp/central_screen/records/ui/allergies/allergies.dart';
+import 'package:doctorapp/central_screen/records/ui/conditions/conditions.dart';
+import 'package:doctorapp/central_screen/records/ui/immunizations/immunizations.dart';
+import 'package:doctorapp/central_screen/records/ui/lab_results/lab_results.dart';
+import 'package:doctorapp/central_screen/records/ui/medications/medications.dart';
+import 'package:doctorapp/central_screen/records/ui/procedures/procedures.dart';
 import 'package:doctorapp/patients/ui/patients.dart';
 import 'package:flutter/material.dart';
 
 import 'auth/ui/auth.dart';
 import 'auth/ui/login.dart';
 import 'auth/ui/sign_up.dart';
+import 'central_screen/care_team/message.dart';
+import 'central_screen/journal/journal_tile.dart';
+import 'central_screen/journal/log_entry_add.dart';
+import 'central_screen/journal/log_entry_note.dart';
+import 'central_screen/journal/log_entry_view.dart';
 import 'central_screen/ui/central_screen.dart';
 
 class Routes {
@@ -12,9 +24,38 @@ class Routes {
   static const auth = 'auth';
   static const signUp = 'auth/sign_up';
   static const login = 'auth/login';
+  static const logEntryAdd = 'central_screen/health/log_entry_add';
+  static const logEntryNote = 'central_screen/health/log_entry_note';
+  static const logEntryView = 'central_screen/health/log_entry_view';
+  static const labResults = 'central_screen/records/lab_results';
+  static const conditions = 'central_screen/records/conditions';
+  static const allergies = 'central_screen/records/allergies';
+  static const immunizations = 'central_screen/records/immunizations';
+  static const medications = 'central_screen/records/medications';
+  static const procedures = 'central_screen/records/procedures';
+  static const message = 'care_team/message';
+  static const profile = 'care_team/profile';
 }
 
 Route<dynamic> generateRoute(RouteSettings settings) {
+  if (settings.name == Routes.logEntryNote) {
+    final EntryArg args = settings.arguments;
+
+    return MaterialPageRoute(
+      builder: (context) {
+        return LogEntryNote(entry: args.entry);
+      },
+    );
+  } else if (settings.name == Routes.logEntryView) {
+    final EntryArg args = settings.arguments;
+
+    return MaterialPageRoute(
+      builder: (context) {
+        return LogEntryView(entry: args.entry);
+      },
+    );
+  }
+
   assert(settings.arguments == null);
   switch (settings.name) {
     case Routes.patients:
@@ -27,6 +68,24 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => SignUp());
     case Routes.login:
       return MaterialPageRoute(builder: (context) => Login());
+    case Routes.logEntryAdd:
+      return MaterialPageRoute(builder: (context) => LogEntryAdd());
+    case Routes.procedures:
+      return MaterialPageRoute(builder: (context) => Procedures());
+    case Routes.medications:
+      return MaterialPageRoute(builder: (context) => Medications());
+    case Routes.immunizations:
+      return MaterialPageRoute(builder: (context) => Immunizations());
+    case Routes.allergies:
+      return MaterialPageRoute(builder: (context) => Allergies());
+    case Routes.conditions:
+      return MaterialPageRoute(builder: (context) => Conditions());
+    case Routes.labResults:
+      return MaterialPageRoute(builder: (context) => LabResults());
+    case Routes.message:
+      return MaterialPageRoute(builder: (context) => Message());
+    case Routes.profile:
+      return MaterialPageRoute(builder: (context) => Profile());
     default:
       return MaterialPageRoute(
           builder: (context) => UndefinedRoute(settings.name));
